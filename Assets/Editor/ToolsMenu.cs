f0f334ab874ee6d78a4af135153a132028387fd1
blizearh <blizearh@gmail.com>
2023-05-10 04:24:20 +0300

    Commit to master

---
+++ Assets/Editor/ToolsMenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace jasonstorey
+{
+    public static class ToolsMenu
+    {
+        [MenuItem("Tools/Setup/Create Default Folders")]
+        public static void CreateDefaultFolders()
+        {
+
+        }
+        
+    }
+}
